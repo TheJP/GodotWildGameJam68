550c4c200cefd4272e9e2ddb73ef1f287d3efaa0
Robin Renggli <robin.k.renggli@gmail.com>
2024-04-13 20:21:01 +0200

    add global ticker, add basic character movement

---
+++ Godot/globals/ticker.gd
@@ -0,0 +1,7 @@
+extends Node
+ 
+var timer = Timer.new()  
+  
+func _ready():  
+	add_child(timer)  
+	timer.start(1)  
---
+++ Godot/scenes/fighting/Enemy.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@onready var ray = $RayCast2D
+
+var tile_size = 64
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Ticker.timer.timeout.connect(on_global_ticker_timeout) 
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+func on_global_ticker_timeout():
+	move()
+
+func move():
+	ray.target_position = Vector2.LEFT * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += Vector2.LEFT * tile_size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
