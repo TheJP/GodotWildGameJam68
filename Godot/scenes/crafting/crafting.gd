fd8875e02d7c96214888f0262bf94993a83440bd
TheJP <jp_55555@hotmail.com>
2024-04-16 20:02:59 +0200

    Changed tilesize to 100x100

--- Godot/scenes/crafting/crafting.gd
+++ Godot/scenes/crafting/crafting.gd
@@ -2,9 +2,32 @@ extends Node2D
 
 
 @onready var _menu: MenuInGame = get_tree().get_first_node_in_group("menu_in_game")
+@onready var _build_tool_scene = preload('res://scenes/crafting/build_tool.tscn')
+
+
+var _current_tool = null
 
 
 func _ready():
 	if _menu == null:
 		push_error('crafting scene could not connect to menu')
 		return
+
+	_menu.start_default_tool.connect(_before_tool_switch)
+	_menu.start_build_crafter.connect(_start_building.bind(Tile.Type.CRAFTER))
+	_menu.start_build_pipe.connect(_start_building.bind(Tile.Type.PIPE))
+	_menu.start_build_trash.connect(_start_building.bind(Tile.Type.TRASH_CAN))
+	_menu.start_remove.connect(_before_tool_switch)
+
+
+func _start_building(type: Tile.Type):
+	_before_tool_switch()
+	_current_tool = _build_tool_scene.instantiate()
+	_current_tool.type = type
+	add_child(_current_tool)
+
+
+func _before_tool_switch():
+	if _current_tool != null:
+		_current_tool.queue_free()
+		_current_tool = null
--- Godot/scenes/fighting/fighting.gd
+++ Godot/scenes/fighting/fighting.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+
+@onready var tilesize = GameParameters.tilesize
+
+
 func _draw():
 	var size = get_viewport_rect().size
 	var g = global_position
-	for i in range(int((- size.x) / 64) - 1, int((size.x) / 64) + 1):
-		draw_line(Vector2(i * 64, size.y + 100) - g, Vector2(i * 64, -size.y-100) - g, Color.BLACK)
-	for i in range(int((- size.y) / 64) - 1, int((size.y) / 64) + 1):
-		draw_line(Vector2(size.x + 100, i * 64) - g, Vector2(- size.x - 100, i * 64) - g, Color.BLACK)
+	for i in range(int((- size.x) / tilesize) - 1, int((size.x) / tilesize) + 1):
+		draw_line(Vector2(i * tilesize, size.y + 100) - g, Vector2(i * tilesize, -size.y-100) - g, Color.BLACK)
+	for i in range(int((- size.y) / tilesize) - 1, int((size.y) / tilesize) + 1):
+		draw_line(Vector2(size.x + 100, i * tilesize) - g, Vector2(- size.x - 100, i * tilesize) - g, Color.BLACK)
