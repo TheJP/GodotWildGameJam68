1a8121c374a3d3b06b2d4f7c3b4e3a8a5696535c
Robin Renggli <robin.k.renggli@gmail.com>
2024-04-19 19:09:13 +0200

    fix damage scaling bug

--- Godot/scenes/fighting/enemySpawner.gd
+++ Godot/scenes/fighting/enemySpawner.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var enemy = preload("res://scenes/fighting/enemy.tscn")
 var spawn_rate = 36
 var counter = 35
-var times_spawned = 0
+var times_spawned = 1
 var increase_health_frequency = 2
 var increase_damage_frequency = 5
 var health_bonus = 0
@@ -22,11 +22,11 @@ func on_global_ticker_timeout():
 		enemy_instance.increase_health(health_bonus)
 		enemy_instance.damage += damage_bonus
 		enemy_instance.global_position = self.global_position
-		times_spawned += 1
-		if times_spawned-1 % increase_health_frequency == 0:
+		if times_spawned % increase_health_frequency == 0:
 			health_bonus += 2
-		if times_spawned-1 % increase_health_frequency == 0:
+		if times_spawned % increase_health_frequency == 0:
 			damage_bonus += 1
+		times_spawned += 1
 		counter = 0
 
 func _process(_delta):
