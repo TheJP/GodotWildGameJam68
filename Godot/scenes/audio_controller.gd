f5bb52e211b708548d133957c855b60073bfc70e
TheJP <jp_55555@hotmail.com>
2024-04-19 14:34:21 +0200

    Fix: AudioController (hopefully last fix now)

--- Godot/scenes/audio_controller.gd
+++ Godot/scenes/audio_controller.gd
@@ -5,9 +5,13 @@ extends Node
 static var controller: AudioController
 
 
+static func get_player(path: NodePath) -> AudioStreamPlayer:
+	return controller._get_player(path)
+
+
 func _ready():
 	controller = self
 
 
-static func get_player(path: NodePath) -> AudioStreamPlayer:
-	return controller.get_node(path) as AudioStreamPlayer
+func _get_player(path: NodePath) -> AudioStreamPlayer:
+	return get_node(path) as AudioStreamPlayer
