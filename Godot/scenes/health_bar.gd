1958fe6204d895f11031de03f92000429f5dd96b
TheJP <jp_55555@hotmail.com>
2024-04-20 23:58:24 +0200

    Updated healthbar visuals

--- Godot/scenes/health_bar.gd
+++ Godot/scenes/health_bar.gd
@@ -4,6 +4,6 @@ func _ready():
 	GlobalStats.update_health_bar.connect(on_update)
 	max_value = GlobalStats.factory_health
 	value = GlobalStats.factory_health
-	
+
 func on_update():
 	value = GlobalStats.factory_health
