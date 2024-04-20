23cf21a703ff01ac2207055927a5f44fd1188097
TheJP <jp_55555@hotmail.com>
2024-04-20 20:37:37 +0200

    Added tutorial 2 and 3

--- Godot/scenes/ui/tutorial.gd
+++ Godot/scenes/ui/tutorial.gd
@@ -3,11 +3,15 @@ extends Control
 
 static var _texts: Array[String] = [
 	'Build a factory to craft items.',
+	'Drag pipes to make connections.',
+	'Equip your fighters to defend against attacking monsters.'
 ]
 
 
 static var _videos: Array = [
 	preload("res://video/tutorial_1.ogv"),
+	preload("res://video/tutorial_2.ogv"),
+	preload("res://video/tutorial_3.ogv"),
 ]
 
 
@@ -16,7 +20,6 @@ var _current_index := 0
 
 func _ready():
 	assert(len(_texts) == len(_videos), 'invalid tutorial setup')
-	#GameParameters.is_tutorial = true # TODO: remove
 	visible = GameParameters.is_tutorial
 	get_tree().paused = visible
 	if not GameParameters.is_tutorial:
@@ -25,10 +28,24 @@ func _ready():
 	_update_tutorial()
 
 
+func _on_back_button_pressed():
+	_current_index = clampi(_current_index - 1, 0, len(_videos) - 1)
+	_update_tutorial()
+
+
+func _on_next_button_pressed():
+	if _current_index + 1 >= len(_videos):
+		visible = false
+		get_tree().paused = false
+	else:
+		_current_index = clampi(_current_index + 1, 0, len(_videos) - 1)
+		_update_tutorial()
+
+
 func _update_tutorial():
 	%Text.text = _texts[_current_index]
 	%VideoStreamPlayer.stream = _videos[_current_index]
 	%VideoStreamPlayer.play()
 	%BackButton.visible = _current_index > 0
+	%NextButton.text = 'Next' if _current_index + 1 < len(_videos) else 'Close'
 	%PageNumber.text = '{0} / {1}'.format([_current_index + 1, len(_videos)])
-
