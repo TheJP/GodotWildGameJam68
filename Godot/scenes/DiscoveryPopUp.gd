7c61f265d711cef15fc5cd6e72974bee86dc6170
velvence <g.wiederkehr@outlook.com>
2024-04-20 18:02:06 +0200

    fix from mr

--- Godot/scenes/DiscoveryPopUp.gd
+++ Godot/scenes/DiscoveryPopUp.gd
@@ -1,13 +1,7 @@
 extends Control
 
-<<<<<<< HEAD
 @onready var sprite = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Sprite
 @onready var label = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Label2
-@onready var _animation_player = $AnimationPlayer
-=======
-@onready var sprite = %ItemSprite
-@onready var label = %ItemName
->>>>>>> fa9a8e22359222fcf97739df485afc4818db4b06
 
 var discovery_queue = []
 
@@ -27,7 +21,6 @@ func _on_discovery(type):
 		label.text = Item.names[type]
 		self.visible = true
 		AudioController.get_player("ItemDiscoverySound").play()
-		_animation_player.play("party_fireworks")
 		get_tree().paused = true
 	else:
 		discovery_queue.append(type)
