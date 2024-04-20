605138abdefd63e5fcec587bb351639572db50e5
TheJP <jp_55555@hotmail.com>
2024-04-20 22:56:41 +0200

    Added background (top) to game and made grid lines less strong

--- Godot/scenes/fighting/fighting.gd
+++ Godot/scenes/fighting/fighting.gd
@@ -8,6 +8,6 @@ func _draw():
 	var size = get_viewport_rect().size
 	var g = global_position
 	for i in range(int((- size.x) / tilesize) - 1, int((size.x) / tilesize) + 1):
-		draw_line(Vector2(i * tilesize, size.y + 100) - g, Vector2(i * tilesize, -size.y-100) - g, Color.BLACK)
+		draw_line(Vector2(i * tilesize, size.y + 100) - g, Vector2(i * tilesize, -size.y-100) - g, Color(Color.BLACK, 0.15))
 	for i in range(int((- size.y) / tilesize) - 1, int((size.y) / tilesize) + 1):
-		draw_line(Vector2(size.x + 100, i * tilesize) - g, Vector2(- size.x - 100, i * tilesize) - g, Color.BLACK)
+		draw_line(Vector2(size.x + 100, i * tilesize) - g, Vector2(- size.x - 100, i * tilesize) - g, Color(Color.BLACK, 0.15))
