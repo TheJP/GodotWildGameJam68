8d2f6fa411cf3a39ed4d567986e5d9da50c710a7
Robin Renggli <robin.k.renggli@gmail.com>
2024-04-20 15:22:20 +0200

    fix discovering multiple items in same tick

--- Godot/scenes/DiscoveryPopUp.gd
+++ Godot/scenes/DiscoveryPopUp.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var sprite = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Sprite
 @onready var label = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Label2
 
+var discovery_queue = []
+
 func _ready():
 	ItemDiscovery.discovery.connect(_on_discovery)
 	self.visible = false
@@ -10,10 +12,15 @@ func _ready():
 func _on_button_pressed():
 	self.visible = false
 	get_tree().paused = false
+	if !discovery_queue.is_empty():
+		_on_discovery(discovery_queue.pop_front())
 	
 func _on_discovery(type):
-	sprite.texture = Item.sprites[type]
-	label.text = Item.names[type]
-	self.visible = true
-	AudioController.get_player("ItemDiscoverySound").play()
-	get_tree().paused = true
+	if(self.visible == false):
+		sprite.texture = Item.sprites[type]
+		label.text = Item.names[type]
+		self.visible = true
+		AudioController.get_player("ItemDiscoverySound").play()
+		get_tree().paused = true
+	else:
+		discovery_queue.append(type)
