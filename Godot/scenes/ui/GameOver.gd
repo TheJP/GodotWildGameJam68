da25409034fea24d10c25f66eb0ae11d7dca12ac
TheJP <jp_55555@hotmail.com>
2024-04-21 21:43:21 +0200

    Added sounds to game over scene

--- Godot/scenes/ui/GameOver.gd
+++ Godot/scenes/ui/GameOver.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+
+var _hovered = null
+
+
 func _ready():
 	var score = GlobalStats.times_spawned
 	var highscore = GlobalStats.highscore
@@ -15,5 +19,15 @@ func _ready():
 func _on_button_pressed():
 	GlobalStats.new_game()
 	AudioController.get_player("Level1Loop").stream.set_loop_mode(1)
-	AudioController.get_player("Level1Loop").start()
+	AudioController.get_player("Level1Loop").play()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _button_hovered(control: String):
+	if _hovered != control:
+		_hovered = control
+		AudioController.get_player("MenuHoverSound").play()
+
+
+func _button_unhovered():
+	_hovered = null
