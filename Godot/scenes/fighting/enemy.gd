1c918285deb761214517c1b118b7f53725c3a583
TheJP <jp_55555@hotmail.com>
2024-08-06 01:33:17 +0200

    Enemies damage village when they reach it

--- Godot/scenes/fighting/enemy.gd
+++ Godot/scenes/fighting/enemy.gd
@@ -12,6 +12,7 @@ const sounds_damage = ['DamageTickSound1', 'DamageTickSound2', 'DamageTickSound3
 var _path: Array[Vector2i] = []
 var _path_index := 0
 var _current_road = null
+var _village_index: Vector2i
 var _hovering = false
 
 var health = 20
@@ -20,6 +21,7 @@ var move_frequency = 2
 var counter = 0
 var is_plant = false
 var animation_speed = 3
+var village_damage = 10
 
 
 func _ready():
@@ -30,11 +32,19 @@ func _ready():
 		push_warning('enemy did not spawn on road or road was already occupied')
 
 	# TODO: Improve performance by not searching village with each enemy.
+	PathFinding.layout_changed.connect(on_road_layout_changed)
 	var village = get_tree().get_first_node_in_group('village')
-	var village_index = Tile.index(village.global_position) - Vector2i.ONE
-	_path = PathFinding.find_path(index, village_index, Vector2i(3, 3)) # TODO: magic numbers
+	_village_index = Tile.index(village.global_position) - Vector2i.ONE
+	_path = PathFinding.find_path(index, _village_index, Vector2i(3, 3)) # TODO: magic numbers
+	_path_index = 1
 
 	Game.timer.timeout.connect(_on_global_ticker_timeout)
+
+
+func on_road_layout_changed():
+	var index := Tile.index(global_position)
+	_path = PathFinding.find_path(index, _village_index, Vector2i(3, 3)) # TODO: magic numbers
+	_path_index = 1
 
 
 func _on_global_ticker_timeout():
@@ -44,9 +54,36 @@ func _on_global_ticker_timeout():
 	counter = 0
 
 	if _path_index < _path.size():
-		var target_position := Tile.position(_path[_path_index])
-		create_tween().tween_property(self, "global_position",
+		var target_index := _path[_path_index]
+		var target_position := Tile.position(target_index)
+		if _path_index + 1 == _path.size():
+			# Reached village -> deal damage.
+			GlobalStats.update_health(-village_damage)
+			if is_instance_valid(_current_road):
+				_current_road.fighter = null
+
+			var tween := create_tween()
+			tween.set_parallel()
+			tween.tween_property(self, 'global_position',
+				target_position, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(self, 'scale',
+				0.5 * Vector2.ONE, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+			await tween.finished
+
+			queue_free()
+			return
+
+		var target_road: Road = PathFinding.get_road(target_index)
+		if target_road == null or is_instance_valid(target_road.fighter):
+			return
+		if is_instance_valid(_current_road):
+			_current_road.fighter = null
+		target_road.fighter = self
+		_current_road = target_road
+
+		create_tween().tween_property(self, 'global_position',
 			target_position, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+
 		_path_index += 1
 
 
